c9fa2e3463d3c62d0f6fbdf8c409da9bf8421742
sosenenterprenuer-cloud <sosenenterprenuer@gmail.com>
2025-10-20 09:20:56 +0800

    Delete fyp-cursor-main directory

--- fyp-cursor-main/scripts/smoke_test.py
+++
@@ -1,8 +0,0 @@
-import importlib, os, sys, subprocess
-print("[SMOKE] Python compile OK.")
-# bank validator
-rc = subprocess.call([sys.executable, "scripts/enforce_quiz_bank_30.py"])
-if rc != 0:
-    sys.exit(rc)
-print("[SMOKE] Bank validator passed.")
-
